c014ef4555699faa4577ed9974e5187a792fa099
Bruce <BruceJiang@qq.com>
2022-08-20 18:25:29 +0800

    hot-fix first commit

--- src/main/java/com/study/demo/Demo.java
+++ src/main/java/com/study/demo/Demo.java
@@ -5,5 +5,6 @@ public class Demo {
         System.out.println("idea中安装git进行测试提交！");
         System.out.println("第二次提交到暂存区！");
         System.out.println("第三次提交到暂存区！");
+        System.out.println("hot-fix提交到本地库");
     }
 }
