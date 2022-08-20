0556385ad89f2daa29714f524fb547a238eb34e9
Bruce <BruceJiang@qq.com>
2022-08-20 18:39:42 +0800

    hot-fix second commit

--- src/main/java/com/study/demo/Demo.java
+++ src/main/java/com/study/demo/Demo.java
@@ -2,9 +2,10 @@ package com.study.demo;
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("idea中安装git进行测试提交！");
-        System.out.println("第二次提交到暂存区！");
-        System.out.println("第三次提交到暂存区！");
+        System.out.println("hot-fix 第一次提交本地库！");
+        System.out.println("hot-fix 第二次提交本地库！");
+        System.out.println("hot-fix 第三次提交本地库！");
         System.out.println("hot-fix提交到本地库");
+        System.out.println("hot-fix 第四次提交本地库！");
     }
 }
