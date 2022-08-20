3e752c992ebd2db70128338b0a2bec3879f838b5
Bruce <BruceJiang@qq.com>
2022-08-20 17:51:45 +0800

    second commit

--- src/main/java/com/study/demo/Demo.java
+++ src/main/java/com/study/demo/Demo.java
@@ -3,5 +3,6 @@ package com.study.demo;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("idea中安装git进行测试提交！");
+        System.out.println("第二次提交到暂存区！");
     }
 }
